3b32b0b10e51d5522aa60aed610e3cb4be2a253d
Егор Львов <workregor@mail.ru>
2025-04-11 23:45:04 +0300

    license update

--- exclumon/src/main.cpp
+++ exclumon/src/main.cpp
@@ -1,15 +1,27 @@
+/*******************************************************************************
+    Exclusion Monitor - мониторинг исключений Windows Defender
+    Copyright (C) 2025 Egor Lvov
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*******************************************************************************/
+
 #include <Windows.h>
-#include <minwindef.h>
-#include <processenv.h>
-#include <shellapi.h>
 #include <stdio.h>
 #include <algorithm>
 #include <iterator>
 #include <string>
 #include <format>
-#include <windef.h>
-#include <winnt.h>
-#include <winuser.h>
 #include "reg.hpp"
 #include "notify.hpp"
 #include "resource.h"
